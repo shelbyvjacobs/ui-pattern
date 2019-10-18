73d5126acf174035b34ca3e3e6f2c380c9ad9189
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-18 16:48:16 -0400

    tried to fix modal close button

--- script.js
+++ script.js
@@ -5,54 +5,45 @@ const descriptionDiv = document.querySelectorAll(".desc")
 const modal = document.querySelector("#modal1")
 const modalImg = document.querySelector("#modalImg1")
 const photoDiv1 = document.querySelector(".photo1")
+const closeButton = document.querySelectorAll(".close")[0];
+const accordian = document.querySelector(".accordian")
+let i = 0;
 
 //apod
-	fetch(url)
-		.then(res => res.json())
-		.then(res => {
-			// console.log(res)
-			// console.log(res.hdurl)
-			let photo1 = document.createElement("IMG");
+fetch(url)
+	.then(res => res.json())
+	.then(res => {
+		let photo1 = document.createElement("IMG");
 			photo1.setAttribute ("src", res.hdurl);
 			photo1.setAttribute ("class", "apod1")
 			photoDiv1.appendChild(photo1)
-			//modal
-			const i = 0;
-			photo[i].addEventListener("click", function(evt){
+		//modal
+		photo[i].addEventListener("click", function(evt){
 			evt.preventDefault();
-			console.log(res);
 			console.log("photo clicked!")
-		//on click, open modal showing the image blown up.
 			modal.style.display = "block";
-			modalImg.src = res.hdurl; //link it to the image from nasa again
-	})
+			modalImg.src = res.hdurl;
 		})
-		.catch(err => console.log("err", err))
-
-
+	})
+	.catch(err => console.log("err", err))
 
 
 //close modal
-const closeButton = document.querySelectorAll(".close");
-
-for(let i=0; i<closeButton.length; i++){
-	closeButton[i].addEventListener("click", function(evt){
-		evt.preventDefault();
-		console.log("clicked close")
-		modal.style.display = "none";
-	})
-};
+closeButton.addEventListener("click", function(evt){
+	evt.preventDefault();
+	console.log("clicked close")
+	modal.style.display = "none";
+}); //maybe it's not closing because when you click on the X it triggers the closeButton and the photo eventListeners at the same time?
+//Cancels itself out???
 
 //accordian
-const accordian = document.querySelector(".accordian")
-
-	accordian.addEventListener("click", function(evt){
-		this.classList.toggle("active");
-		const panel = this.nextElementSibling;
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
-		} else {
-			panel.style.display = "block";
-		}
-	});
+accordian.addEventListener("click", function(evt){
+	this.classList.toggle("active");
+	const panel = this.nextElementSibling;
+	if (panel.style.display === "block") {
+		panel.style.display = "none";
+	} else {
+		panel.style.display = "block";
+	}
+});
 
