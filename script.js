6a7288c05c5db5011b33d070203065e16ef83d8e
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-19 15:24:32 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const url = "https://api.nasa.gov/planetary/apod?api_key=yq5ylhafGMwftv9B8A0E753s7LXQeo9TBCh4N0Q3";
 
 const photo = document.querySelectorAll(".photo");
-const descriptionDiv = document.querySelector(".desc")
-const modal = document.querySelector("#modal1")
-const modalImg = document.querySelector("#modalImg1")
-const photoDiv1 = document.querySelector(".photo1")
+const descriptionDiv = document.querySelector(".desc");
+const modal = document.querySelector("#modal1");
+const modalImg = document.querySelector("#modalImg1");
+const photoDiv1 = document.querySelector(".photo1");
 const closeButton = document.querySelectorAll(".close")[0];
-const accordian = document.querySelector(".accordian")
+const accordian = document.querySelector(".accordian");
 const panel = document.querySelector(".panel");
 let i = 0;
 
-//apod
+//from NASA APOD API
 fetch(url)
 	.then(res => res.json())
 	.then(res => {
+		//photo
 		let photo1 = document.createElement("IMG");
 			photo1.setAttribute ("src", res.hdurl);
 			photo1.setAttribute ("class", "apod1");
@@ -48,18 +49,12 @@ closeButton.addEventListener("click", function(evt){
 	evt.preventDefault();
 	console.log("clicked close")
 	modal.style.display = "none";
-}); //maybe it's not closing because when you click on the X it triggers the closeButton and the photo eventListeners at the same time?
-//Cancels itself out???
+});
 
 //accordian
 accordian.addEventListener("click", function(evt){
 	this.classList.toggle("active");
 	const panel = this.nextElementSibling;
-	// if (panel.style.display === "block") {
-	// 	panel.style.display = "none";
-	// } else {
-	// 	panel.style.display = "block";
-	// }
 	if (panel.style.maxHeight) {
     	panel.style.maxHeight = null;
     } else {
